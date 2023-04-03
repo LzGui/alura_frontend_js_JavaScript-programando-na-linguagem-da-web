cb5c548b8a9b13a8428b51f8c2ef13beb64f1cca
Guilherme Costa <guilherme.pimentel@solutions.com>
2023-04-03 16:55:30 -0300

    adicionando funcoes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Calcula o imc
+
 var paciente = document.querySelectorAll(".paciente");
 
 for (let i = 0; i < paciente.length; i++) {
@@ -21,40 +23,14 @@ for (let i = 0; i < paciente.length; i++) {
     }
 
     if (vPeso && vAltura) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     }
 }
 
-var botaoAdd = document.querySelector("#adicionar-paciente");
+function calculaImc (peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
 
-botaoAdd.addEventListener("click", function (event) {
-    event.preventDefault();
-    var form = document.querySelector("#form-adiciona");
-
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-})
+    return imc.toFixed(2);
+}
