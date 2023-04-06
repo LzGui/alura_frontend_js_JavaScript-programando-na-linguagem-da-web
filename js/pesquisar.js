abc3c11ae4e07c81f3581b710d412dd8d91fd614
Guilherme Costa <guilherme.pimentel@solutions.com>
2023-04-06 16:50:10 -0300

    add remoção do paciente

---
+++ js/pesquisar.js
@@ -0,0 +1,11 @@
+var campoPesquisa = document.querySelector("#filtrar-tabela");
+
+campoPesquisa.addEventListener("input", function () {
+    var pacientes = document.querySelectorAll(".pacientes");
+
+    for (var i = 0; i < pacientes.length; i++) {
+        var pacientes = pacientes[i];
+        var tdNome = paciente.querySelector(".info-nome");
+        var nome = paciente.textContent;
+    }
+});
